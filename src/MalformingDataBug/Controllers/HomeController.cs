02abf8b3cccbf4b560b46e56478c2b5855a44316
Mahmut Jomaa <mahmut.jomaa@yahoo.de>
2015-10-02 16:33:01 +0200

    Initial

---
+++ src/MalformingDataBug/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNet.Mvc;
+
+namespace MalformingDataBug.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        [Route("")]
+        public ViewResult Index() => View();
+
+        [HttpPost]
+        [Route("/callback/{id?}")]
+        public JsonResult CallBack(string data) =>Json(new{data2 = data});
+    }
+}
---
+++ src/MalformingDataBug/Startup.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNet.Builder;
+using Microsoft.Framework.DependencyInjection;
+
+namespace MalformingDataBug
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseStaticFiles();
+            app.UseMvc();
+        }
+    }
+}
